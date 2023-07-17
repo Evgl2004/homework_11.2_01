6a6ec3e05ac43d13c45df81beabdee215b1d697d
Andrey Evgl <evgl2004@yandex.ru>
2023-07-17 19:28:47 +0500

    Добавлен файл "str_func.py", в котором реализована функция "str_upper"

---
+++ str_func.py
@@ -0,0 +1,8 @@
+def str_upper(input_str):
+    """
+    Принимает на вход строку и возвращает её со всеми заглавными буквами.
+    :param input_str: Строка
+    :return:
+    """
+    return input_str.upper()
+
