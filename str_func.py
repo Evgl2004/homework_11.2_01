3f677b0dc311dcbdb070610cae53cca51d13e7e3
Andrey Evgl <evgl2004@yandex.ru>
2023-07-17 19:49:57 +0500

    Обновлён файл "str_func.py", добавлена функция "str_title"

--- str_func.py
+++ str_func.py
@@ -1,8 +1,17 @@
 def str_upper(input_str):
     """
     Принимает на вход строку и возвращает её со всеми заглавными буквами.
+    Описание было добавлено изначально с функцией =)
     :param input_str: Строка
     :return:
     """
     return input_str.upper()
 
+
+def str_title(input_str):
+    """
+     Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции.
+    :param input_str: Строка
+    :return:
+    """
+    return input_str.title()
